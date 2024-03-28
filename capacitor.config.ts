9f83b6d63880944a020febeaea34a79a86630554
stevebstorm <steve.lorent@gmail.Com>
2024-03-28 09:55:17 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'I3WadDemoIonic',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
